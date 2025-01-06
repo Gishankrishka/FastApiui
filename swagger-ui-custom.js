05b0ee3e656808ff1f3bba9ee16d3ae4c2025a9c
Gishan <91087239+Gishankrishka@users.noreply.github.com>
2025-01-06 12:18:14 +0530

    Update swagger-ui-custom.js

--- swagger-ui-custom.js
+++ swagger-ui-custom.js
@@ -1,14 +1,3 @@
-
-    window.onload = function() {
-        // Replace top bar with custom logo
-        const topbar = document.querySelector('.swagger-ui .topbar');
-        if (topbar) {
-            topbar.innerHTML = `
-                <div style="display: flex; align-items: center; padding: 0 20px;">
-                    <img src="./logo.png" alt="Logo" style="height: 40px; margin-right: 10px;">
-                    <a href="https://t.me/cyberline_official" style="font-size: 18px; text-decoration: none; color: white;">Cyberline API</a>
-                </div>
-            `;
-        }
-    };
-    
+window.onload = function () {
+    document.body.style.color = "#fff";
+};
